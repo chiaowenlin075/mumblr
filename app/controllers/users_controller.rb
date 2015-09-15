ea82aabbffee4b5c7b213a26a81b664d078fe4f8
Leah Lin <chiaowenlin075@gmail.com>
2015-09-14 20:53:32 -0400

    Finish User#create/#update/#destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
-class UsersController < ApplicationController
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login_user!(@user)
+      redirect_to root_url
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_url
+    else
+      render json: current_user.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    current_user.destroy!
+    redirect_to new_session_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
